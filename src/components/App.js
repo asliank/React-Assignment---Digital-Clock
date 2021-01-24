e0aa81d6b20e6c36221e1142acd79e595104ec01
Ankur Malviya <ankur.gyrix@gmail.com>
2021-01-24 18:38:25 +0530

    logic added

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,46 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {  time: new Date()}
+        this.intervalId = null;
+    }
+    getTimeString() {
+        const currTime = this.state.time
+        const [hours,minutes,seconds] = [
+            currTime.getHours(),
+            currTime.getMinutes(),
+            currTime.getSeconds()
+        ]
+        const amOrPm = hours>=12? "PM" : "AM"
+        const hourFormat = hours > 12 ? hours - 12 : hours
+        const hourString = this.padNumberToTwoDigits(hourFormat)
+        const minutesString = this.padNumberToTwoDigits(minutes)
+        const secondString = this.padNumberToTwoDigits(seconds)
+
+        const timeString = `${hourString}:${minutesString}:${secondString} ${amOrPm}`
+        return timeString
+    }
+
+    padNumberToTwoDigits(num){
+        return (num<10 ? ("0" + num) : (""+ num));
+    }
+
+    componentDidMount(){
+        this.intervalId = setInterval(()=>{
+            this.setState({
+                time:new Date()
+            })
+        })
+    }
     render() {
 
+        
         return(
-            <>
-               
-            </>
+            <div className="Clock">
+               <h3 id="time">{this.getTimeString()}</h3>
+            </div>
         )
     }
 }
